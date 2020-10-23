95a0c7a60f505b1d2cd4f26e054fcb96642f960c
unknown <alejomartinez8@gmail.com>
2020-10-22 22:56:52 -0500

    Add try catch to teams.service.js

--- api/teams/teams.service.js
+++ api/teams/teams.service.js
@@ -16,151 +16,180 @@ module.exports = {
 /** Add an Team */
 async function addTeam(params) {
   try {
-    console.log(params);
-
     if (await db.Team.findOne({ name: params.name })) {
       throw `Equipo "${params.name}" ya está registrado`;
     }
 
     const event = await db.Event.findOne({ _id: params.event });
-    console.log(event)
     if (!event) {
       throw `Error al registrar equipo, no existe el evento relacionado`;
     }
-
-    // if(await db.Challenge.findOne({challenge: challenge.event})){
-    //   throw `Error al registrar equipo, no existe el evento relacionado`
-    // }
+    const challenge = await db.Challenge.findOne({ _id: params.event });
+    if (!challenge) {
+      throw `Error al registrar equipo, no existe el challenge relacionado`;
+    }
 
     const team = new db.Team(params);
     await team.save();
     return team;
   } catch (error) {
-    console.log('catch', error);
     throw error;
   }
 }
 
 /** Update an Team */
 async function updateTeam(id, params) {
-  if (!db.isValidId(id)) throw "Id de equipo no válido";
-  const team = await db.Team.findById(id);
-  if (!team) {
-    throw "Equipo no encontrado";
+  try {
+    if (!db.isValidId(id)) throw "Id de equipo no válido";
+    const team = await db.Team.findById(id);
+    if (!team) {
+      throw "Equipo no encontrado";
+    }
+
+    if (
+      team.name !== params.name &&
+      (await db.Team.findOne({ name: params.name }))
+    ) {
+      throw `Equipo "${params.name}" ya existe`;
+    }
+
+    Object.assign(team, params);
+    team.updated = Date.now();
+    await team.save();
+    return team;
+  } catch (error) {
+    throw error;
   }
-
-  if (
-    team.name !== params.name &&
-    (await db.Team.findOne({ name: params.name }))
-  ) {
-    throw `Equipo "${params.name}" ya existe`;
-  }
-
-  Object.assign(team, params);
-  team.updated = Date.now();
-  await team.save();
-
-  return team;
 }
 
 /** Register Team */
 async function registerTeam(id) {
-  if (!db.isValidId(id)) {
-    throw "Id de equipo no válido";
-  }
+  try {
+    if (!db.isValidId(id)) {
+      throw "Id de equipo no válido";
+    }
 
-  const team = await db.Team.findById(id);
-  if (!team) {
-    throw "Equipo no encontrado";
+    const team = await db.Team.findById(id);
+    if (!team) {
+      throw "Equipo no encontrado";
+    }
+    team.registered = !team.registered;
+    team.updated = Date.now();
+    team.save();
+    return team;
+  } catch (error) {
+    throw error;
   }
-  team.registered = !team.registered;
-  team.updated = Date.now();
-  team.save();
-  return team;
 }
 
 /** Get Teams */
 async function getTeams(query) {
-  const teams = await db.Team.find(query)
-    .populate("challenge")
-    .populate("event")
-    .exec();
-  return teams;
+  try {
+    const teams = await db.Team.find(query)
+      .populate("challenge")
+      .populate("event")
+      .exec();
+    return teams;
+  } catch (error) {
+    throw error;
+  }
 }
 
 /** Get Team by Id */
 async function getTeamById(id) {
-  if (!db.isValidId(id)) throw "Id de equipo no válido";
-  const team = await db.Team.findById(id);
-  if (!team) throw "Equipo no encontrado";
-  return team;
+  try {
+    if (!db.isValidId(id)) throw "Id de equipo no válido";
+    const team = await db.Team.findById(id);
+    if (!team) throw "Equipo no encontrado";
+    return team;
+  } catch (error) {
+    throw error;
+  }
 }
 
 /****************Delete Team by Id **************/
 async function deleteTeam(id) {
-  if (!db.isValidId(id)) {
-    throw "Id no válido";
+  try {
+    if (!db.isValidId(id)) {
+      throw "Id no válido";
+    }
+
+    const team = await db.Team.findOneAndDelete({ _id: id });
+    if (!team) {
+      throw "Equipo no encontrado";
+    }
+
+    return {
+      type: "delete-success",
+      message: "Equipo Eliminado Exitosamente",
+      team: team,
+    };
+  } catch (error) {
+    throw error;
   }
-  const team = await db.Team.findOneAndDelete({ _id: id });
-  if (!team) throw "Equipo no encontrado";
-  return {
-    type: "delete-success",
-    message: "Equipo Eliminado Exitosamente",
-    team: team,
-  };
 }
 
 /** Scores */
 
 /** Add or Update a Turn of a Team */
 async function addScore(id, params) {
-  if (!db.isValidId(id)) {
-    throw "Id de equipo no válido";
+  try {
+    if (!db.isValidId(id)) {
+      throw "Id de equipo no válido";
+    }
+
+    const team = await db.Team.findById(id);
+    if (!team) {
+      throw "Equipo no encontrado";
+    }
+
+    team.turns.push(params);
+    team.updated = Date.now();
+    team.save();
+    return team;
+  } catch (error) {
+    throw error;
   }
-
-  const team = await db.Team.findById(id);
-  if (!team) {
-    throw "Equipo no encontrado";
-  }
-
-  team.turns.push(params);
-  team.updated = Date.now();
-  team.save();
-
-  return team;
 }
 
 /** Update turn/score of a Team */
 async function updateScore(scoreId, params) {
-  // console.log({ scoreId }, { params });
-  if (!db.isValidId(scoreId)) {
-    throw "Id no válido";
-  }
+  try {
+    if (!db.isValidId(scoreId)) {
+      throw "Id no válido";
+    }
 
-  const team = await db.Team.findOne({ "turns._id": scoreId });
-  if (!team) {
-    throw "Score no encontrado";
-  }
+    const team = await db.Team.findOne({ "turns._id": scoreId });
+    if (!team) {
+      throw "Score no encontrado";
+    }
 
-  const idx = team.turns.findIndex((elm) => elm._id == scoreId);
-  if (idx !== -1) {
-    team.turns[idx] = params;
-    team.save();
-    return team.turns[idx];
+    const idx = team.turns.findIndex((elm) => elm._id == scoreId);
+    if (idx !== -1) {
+      team.turns[idx] = params;
+      team.save();
+      return team.turns[idx];
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
 /** Delete turn/score of a Team */
 async function deleteScore(scoreId) {
-  if (!db.isValidId(scoreId)) {
-    throw "Id no válido";
-  }
+  try {
+    if (!db.isValidId(scoreId)) {
+      throw "Id no válido";
+    }
 
-  const team = await db.Team.findOne({ "turns._id": scoreId });
-  if (!team) {
-    throw "Score no encontrado";
-  }
+    const team = await db.Team.findOne({ "turns._id": scoreId });
+    if (!team) {
+      throw "Score no encontrado";
+    }
 
-  team.turns.pull(scoreId);
-  team.save();
+    team.turns.pull(scoreId);
+    team.save();
+  } catch (error) {
+    throw error;
+  }
 }
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 module.exports = errorHandler;
 
 function errorHandler(err, req, res, next) {
-  console.log(err);
+  console.log('errorHandler', err);
   switch (true) {
     case typeof err === 'string':
       // custom application error
